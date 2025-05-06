dcef7246ace5ee008a7482ed92661d02c8c20fb5
deividas strole <deiwis@yahoo.com>
2025-05-05 19:52:30 -0700

    wip

--- venv/main.py
+++ venv/main.py
@@ -6,7 +6,6 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
 from tools import search_tool, wiki_tool, save_tool, save_to_txt
-
 load_dotenv()
 
 class ResearchResponse(BaseModel):
